5752e8df95b940169846065f93525b232106558d
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-03-15 15:53:39 +0000

    homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import MyNavbar from "./components/MyNav";
 import WelcomePage from "./components/WelcomePage";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
       <WelcomePage />
+      <HomePage />
     </div>
   );
 }
