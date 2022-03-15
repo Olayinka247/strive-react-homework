42bed1f6b60f7e9a3ee97af2195277bafba92ad2
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-03-15 15:30:13 +0000

    welcome page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyNavbar from "./components/MyNav";
+import WelcomePage from "./components/WelcomePage";
 
 function App() {
   return (
     <div className="App">
       <MyNavbar />
+      <WelcomePage />
     </div>
   );
 }
