7a91fc0390c66123b0a569366936ea24981f73a7
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-03-15 15:04:22 +0000

    navbar

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,16 @@
+import { Navbar, Nav } from "react-bootstrap";
+
+const MyNavbar = () => (
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Home</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link href="#deets">About</Nav.Link>
+        <Nav.Link href="#deets">Browse</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default MyNavbar;
